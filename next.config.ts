4b516dad20f16bb2edf3d4a9303be1958ee71fcc
Your Name <hassanih97@gmail.com>
2025-11-26 17:32:31 +0000

    defaultLoader@https://9000-firebase-prismaposs-1763998455502.cluster-cbe

--- next.config.ts
+++ next.config.ts
@@ -12,13 +12,13 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*',
+        hostname: '**',
         port: '*',
         pathname: '/**',
       },
       {
         protocol: 'http',
-        hostname: '*',
+        hostname: '**',
         port: '*',
         pathname: '/**',
       },
