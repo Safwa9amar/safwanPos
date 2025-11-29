340e126222eae0af7eaadc91adef8ce8bf0883b5
Bahi Dev <hassanih97@gmail.com>
2025-11-29 18:57:24 +0000

    update prisma.conmfig to use multifile schmea on prismashencafolder

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'prisma/schemas/*.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
