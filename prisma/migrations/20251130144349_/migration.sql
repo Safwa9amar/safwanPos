1fc33dc9362802c471fb34fef9e437d73bdb971d
Bahi Dev <hassanih97@gmail.com>
2025-12-01 11:34:37 +0000

    check all prisma file and table, remove duplicated and add missed tables

--- prisma/migrations/20251130144349_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status` on the `Supplier` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Enum(EnumId(0))`.
-
-*/
--- AlterTable
-ALTER TABLE `Supplier` ADD COLUMN `website` VARCHAR(191) NULL,
-    MODIFY `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL DEFAULT 'ACTIVE';
--- prisma/migrations/20251130144648_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `Product` ADD COLUMN `expiryDate` DATETIME(3) NULL,
-    ADD COLUMN `productionDate` DATETIME(3) NULL;
