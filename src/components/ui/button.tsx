c7963301b47cc5323a5b1f333b296783be042437
Bahi Dev <hassanih97@gmail.com>
2025-12-11 13:04:36 +0000

    Try fixing this error: `Runtime TypeError: i.findDOMNode is not a functi

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -39,7 +40,7 @@ export interface ButtonProps
   asChild?: boolean
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const ButtonComponent = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
@@ -51,6 +52,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   }
 )
-Button.displayName = "Button"
+ButtonComponent.displayName = "Button"
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+    return <ButtonComponent {...props} ref={ref} />;
+});
+Button.displayName = "Button";
+
 
 export { Button, buttonVariants }
