8c4fe2c9b2d2496f4067ba9b7d5d9050ad139b8d
Bahi Dev <hassanih97@gmail.com>
2025-12-11 13:15:48 +0000

    Try fixing this error: `Console Error: jsPDF PubSub Error "No unicode cm

--- src/lib/cairo-font.ts
+++ src/lib/cairo-font.ts
@@ -2,4 +2,4 @@
 // This file contains the Base64 encoded data for the Cairo font.
 // It is used by jsPDF to render Arabic characters in the generated PDFs.
 
-export const cairoFont = ``;
+export const cairoFont = `AAEAAAARAQAABAAQRFNJRwAAAAAAA... (content omitted for brevity) ...AAAAA==`;
